9f67739b4c4107c5f05e809217f3ff576d5e38c4
GustiAzzam <muhammad1azzam@gmail.com>
2024-11-07 10:26:55 +0000

    r4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,21 @@
 import streamlit as st
-import numpy as np
 import pickle
-from tensorflow.keras.models import load_model
 
 @st.cache
-def load_models():
-    lstm_model = None
-    svm_classifier = None
+def load_scaler():
     scaler = None
     error_message = None
 
     try:
-        lstm_model = load_model('/workspaces/blank-app/lstm_model.h5')
-    except OSError as e:
-        error_message = f"Error loading LSTM model: {e}"
-
-    try:
-        with open('/workspaces/blank-app/svm_classifier.pkl', 'rb') as svm_file:
-            svm_classifier = pickle.load(svm_file)
-    except Exception as e:
-        error_message = f"Error loading SVM classifier: {e}"
-
-    try:
         with open('/workspaces/blank-app/scaler.pkl', 'rb') as scaler_file:
             scaler = pickle.load(scaler_file)
-    except Exception as e:
+    except FileNotFoundError as e:
         error_message = f"Error loading scaler: {e}"
 
-    return lstm_model, svm_classifier, scaler, error_message
+    return scaler, error_message
 
-# Memuat model LSTM, SVM, dan Scaler
-lstm_model, svm_classifier, scaler, error_message = load_models()
+# Memuat scaler
+scaler, error_message = load_scaler()
 
 # Jika ada pesan kesalahan, tampilkan dan hentikan eksekusi lebih lanjut
 if error_message:
